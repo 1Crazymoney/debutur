1bbd2bd4135612edd19b49bd9e5b4eb25314bf60
Harsh Singh <hi.harsh@protonmail.ch>
2021-11-06 01:06:57 -0400

    Setup next.js with stitches

---
+++ client/stitches/global.style.ts
@@ -0,0 +1,35 @@
+import { globalCss } from './theme.config'
+
+const globalStyle = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+    scrollBehavior: 'smooth',
+    fontFamily: '$main',
+  },
+  html: {
+    overflowX: 'hidden',
+  },
+  body: {
+    background: '$white9',
+    overflowX: 'hidden',
+  },
+  '::selection': {
+    background: '#4560F060',
+  },
+  img: {
+    userSelect: 'none',
+  },
+  'body::-webkit-scrollbar': {
+    width: 10,
+  },
+  'body::-webkit-scrollbar-track': {
+    background: 'none',
+  },
+  'body::-webkit-scrollbar-thumb': {
+    background: '$main',
+  },
+})
+
+export default globalStyle
---
+++ client/stitches/theme.config.ts
@@ -0,0 +1,33 @@
+import { createStitches } from '@stitches/react'
+
+import { SkyBlue, White } from 'kraftuur'
+
+export const { css, styled, globalCss, getCssText, keyframes } = createStitches(
+  {
+    theme: {
+      colors: {
+        main: '#00ABFF',
+        ...White,
+        ...SkyBlue,
+      },
+      fontSizes: {
+        1: '14px',
+        2: '18px',
+        3: '20px',
+        4: '24px',
+        5: '36px',
+        6: '48px',
+      },
+      fonts: {
+        main: 'Sora, sans-serif',
+      },
+    },
+    media: {
+      iPadPro: '(max-width: 1024px)',
+      iPad: '(max-width: 768px)',
+      iPhonePlus: '(max-width: 414px)',
+      iPhone: '(max-width: 375px)',
+      iPhoneSE: '(max-width: 320px)',
+    },
+  }
+)
