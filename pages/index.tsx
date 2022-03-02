b98bf18ce0aa932c9281506f4c387a807e67252b
Harsh Singh <hi.harsh@protonmail.ch>
2022-03-02 17:53:48 -0500

    Removed unnecessary fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import { GetServerSideProps, NextPage } from 'next'
-import { useRouter } from 'next/router'
-
-import React from 'react'
 
 import SEO from '@components/SEO'
 import LoginComponent from '@components/Login/Login'
@@ -11,26 +8,10 @@ import { Provider } from 'next-auth/providers'
 import Loading from '@components/Loader'
 
 const Login: NextPage<{ providers: Provider }> = ({ providers }) => {
-  const router = useRouter()
-
   const { data: session, status } = useSession()
-  const [done, setDone] = React.useState(false)
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/jwt')
-      res.json().then((json) => {
-        if (json !== null) router.push(`/${json.sub}`)
-        else setDone(true)
-      })
-    }
-
-    fetchData()
-  }, [session])
 
   return (
     <>
-      {done ? (
         <>
           <SEO title="Get Started" description="Create your Debutur account!" />
           <LoginComponent provider={providers} />
